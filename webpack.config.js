72073f572cc24f165629ba9b7c86410781d58dfa
Yongwoo Jung <stegano@naver.com>
2023-09-04 22:11:09 +0900

    feat(build): add `webpack` and `tsconfig` configuration

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  target: "node",
+  entry: {
+    index: path.resolve(__dirname, "src", "index.ts"),
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "bin"),
+    libraryTarget: "amd",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              configFile: path.resolve(__dirname, "tsconfig.build.json"),
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  externals: {
+    react: "react",
+  },
+};
