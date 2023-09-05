f82beaa78f4ad13687fec372225fa4779be688d6
Yongwoo Jung <stegano@naver.com>
2023-09-06 00:09:29 +0900

    fix(webpack.config.js): change libraryTarget value from `amd` to `umd`

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "bin"),
-    libraryTarget: "amd",
+    libraryTarget: "umd",
   },
   module: {
     rules: [
