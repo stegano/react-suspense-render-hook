72e075eb4c72934da1c2b151035f54c86e86e8ea
Yongwoo Jung <stegano@naver.com>
2023-12-20 23:27:03 +0900

    release v2.2.2

--- bin/hooks/use-suspense-render.d.ts
+++ bin/hooks/use-suspense-render.d.ts
@@ -1,3 +1,3 @@
 import { type ReturnValues, Options } from "./use-suspense-render.interface";
-declare const useSuspenseRender: <Data extends unknown = any, TaskError = unknown>(options?: Options<Data>) => ReturnValues<Data, TaskError>;
+declare const useSuspenseRender: <Data extends unknown = any, TaskError = unknown>(options?: Options<Data>, id?: string | undefined) => ReturnValues<Data, TaskError>;
 export default useSuspenseRender;
